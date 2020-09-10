a0e9c727d287495bb54979bdf71654cadf48e579
Mare <sevilamare@gmail.com>
2020-09-10 16:32:57 -0300

    display score and send input

--- src/scenes/end.js
+++ src/scenes/end.js
@@ -10,11 +10,30 @@ export default class gameEnd extends Scene {
     this.add.text(120, 80, 'Game Over', {
       fill: 'red',
       fontSize: 'bold 36px',
-      fontFamily: 'Georgias, Times, serif'
-    })
+      fontFamily: 'Georgias, Times, serif',
+    });
 
     // score
-    const score = `<p>SCORE ${gS.score}<p>`
+    const score = `<p>SCORE ${window.gS.score}<p>`;
     this.add.dom(165, 140).setOrigin(0).createFromHTML(score);
+
+    // SUBMIT SCORE
+    const inputName = `<form>
+    <input style="color: #333;
+      width: 160px;
+      padding: 10px;
+      background: #fff;
+    " id="fieldName">
+
+    <input type="button" value="GO" 
+      id="sendName"
+      style="color: #333;
+      padding: 8px;
+      background: #fff;
+      border: solid 2px #333;
+      cursor: pointer;
+      font-size: 16px0">
+  </form>`;
+    this.add.dom(85, 270).setOrigin(0).createFromHTML(inputName);
   }
 }
