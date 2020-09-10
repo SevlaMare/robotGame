c0f4a31919dec3c8043d5e242c6a62bb107e8102
Mare <sevilamare@gmail.com>
2020-09-10 13:58:06 -0300

    stage decoration

--- src/scenes/main.js
+++ src/scenes/main.js
@@ -2,9 +2,18 @@ export default class gameMain extends Phaser.Scene {
   constructor() { super('startGame'); }
 
   create() {
+    // background
     this.bg = this.add.image(0, 0, 'bg')
       .setOrigin(0, 0)
       .setScale(0.65)
       .setScrollFactor(0);
+
+    // stage decoration
+    this.bg = this.add.image(165, 357, 'tree');
+    this.bush = this.add.image(550, 340, 'bush');
+    this.skull = this.add.image(1000, 465, 'skull');
+    this.cloud = this.add.image(350, 80, 'cloud').setScale(0.4);
+  
+    
   }
 }
