97cbe6365208c9981dc5fc2840805dda93580910
Mare <sevilamare@gmail.com>
2020-09-10 13:05:15 -0300

    title

--- src/scenes/boot.js
+++ src/scenes/boot.js
@@ -35,6 +35,14 @@ export default class gameLoad extends Phaser.Scene {
   };
 
   create() {
+    // background
     this.bg = this.add.tileSprite(200, 300, 400, 600, 'bgForest');
+
+    // title
+    this.add.text(80, 70, 'Robot  vs  Ninja', { 
+      fill: '#345',
+      fontSize: 'bold 36px',
+      fontFamily: 'Georgias, Times, serif'
+    })
   }
 };
