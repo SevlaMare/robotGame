17a9c55d5ac880a9081ab8ec03375c8295e4af5d
Mare <sevilamare@gmail.com>
2020-09-10 17:07:56 -0300

    leadboard container

--- src/scenes/score.js
+++ src/scenes/score.js
@@ -12,5 +12,19 @@ export default class gameScores extends Scene {
       fontSize: 'bold 36px',
       fontFamily: 'Georgias, Times, serif',
     });
+
+    // leadboard
+    const containerOpen = `<div id="scoresBoard" style="
+      height: 350px;
+      width: 250px;
+      padding: 3px 5px;
+      font-size: 16xpx;
+      border: 3px solid #333;
+      border-radius: 5px;
+      color: #333;
+      background: #fff;>
+    "`;
+    let playersScores = '';
+    const containerClose = `</div>`;
   }
 }
