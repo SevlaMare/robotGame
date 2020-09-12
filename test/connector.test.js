45d3cad221bf9efecdbd7cb42ec1eaf19388f9be
Mare <sevilamare@gmail.com>
2020-09-12 13:40:14 -0300

    test conn v1

--- test/connector.test.js
+++ test/connector.test.js
@@ -1,9 +1,19 @@
 import { getScore, sendScore } from '../src/connector';
 
+describe('getScore', () => {
+  const fetchArray = [{ user: 'asd', score: 123 }, { user: 'dsa', score: 321 }]
+  global.fetch = jest.fn(() => Promise.resolve({ json: () => fetchArray}))
 
-const array = [{ user: 'asd', score: 123 }, { user: 'dsa', score: 321 }]
-global.fetch = jest.fn(() => Promise.resolve({ json: () => array}))
+  it('works with async/await and resolves', async () => {
+    await expect(getScore('url')).resolves.toEqual(fetchArray);
+  });
+});
 
-it('works with async/await and resolves', async () => {
-  await expect(getScore('as')).resolves.toEqual(array);
+describe('sendScore', () => {
+  it('works with async/await and resolves', async () => {
+    const fetchData = { user: 'asd', score: 123 }
+    global.fetch = jest.fn(() => Promise.resolve({ json: () => fetchData}))
+
+    await expect(sendScore('url')).resolves.toEqual(fetchData);
+  });
 });
