f39bb0cfb374d3e71d720461f13523274845240d
Mare <sevilamare@gmail.com>
2020-09-14 12:52:54 -0300

    cors for post method

--- src/connector.js
+++ src/connector.js
@@ -14,6 +14,7 @@ const sendScore = async (name, score) => {
 
   const response = await fetch(url, {
     method: 'POST',
+    mode: 'cors',
     headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
     body: JSON.stringify({ user: name, score }),
   });
